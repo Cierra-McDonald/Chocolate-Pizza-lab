83f34495f7a3209cec5cb0ec8a6b87faa741c3c6
Cierra-McDonald <cierralauren15@gmail.com>
2021-02-08 16:37:03 -0700

    header edits

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import logo192 from '../public';
 
 class OurHeader extends React.Component { 
     render() { 
       return ( 
         <div>
-          <h1>Hi, I am the Header!</h1>
+          <h1>Hi! We're making Chocolate Pizza!</h1>
+          <div>
+          <img src= {logo192.png} alt = "" />
+          </div>
+          <div img src= "">pic2</div>
+          <div img src= "">pic3</div>
         </div>
       )
     }
