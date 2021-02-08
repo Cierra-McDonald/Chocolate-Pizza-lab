b2e3f25ef732b4aee558f287c38967a811f5e5e4
Cierra-McDonald <cierralauren15@gmail.com>
2021-02-08 15:53:27 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import {Switch,BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Home from './components/Home'
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <Router>
+          <Switch>
+            <Route path="/Home" component={Home}/>
+            <nav><Link to ='/Home'> Home </Link></nav>
+          </Switch>
+        </Router>
+      <p>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </p>
+      <div>Hello!</div>
       </header>
     </div>
   );
+}
+
+class OurHeader extends React.Component { 
+  render() { 
+    return ( 
+      <div>
+        <h1>Hi, I am the Header!</h1>
+      </div>
+    )
+  }
+}
+
+function OurHeader(props) { 
+  return <h1>Hello, {props.name}</h1>
 }
 
 export default App;
